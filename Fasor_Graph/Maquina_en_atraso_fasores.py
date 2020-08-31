166d9b160d7b07650c4f93a85b0b2effc908250f
DavidUrbaez <durbaezl@unal.edu.co>
2020-08-31 13:18:23 -0500

    Add info en carpeta

--- Fasor_Graph/Maquina_en_atraso_fasores.py
+++ Fasor_Graph/Maquina_en_atraso_fasores.py
@@ -1,44 +1,30 @@
 from Fasor_Graph import vector_r_theta
 import numpy as np
 import matplotlib.pyplot as plt
-
 plt.style.use('dark_background')
 
-Va = vector_r_theta(4, 0, 'r')
-Vb = vector_r_theta(4, 120, 'g')
-Vc = vector_r_theta(4, 240, 'b')
+angle_V_I = 10
+Ra = 2
+Xa = 4
 
-Vab = Va - Vb
-Vca = Vc - Va
-Vbc = Vb - Vc
+Va = vector_r_theta(6, 0, 'r')
+Ia = vector_r_theta(2, angle_V_I, 'c')
+VRA = Ia * Ra
+VXA = Ia * Xa * 1j
+VRA.color, VXA.color = 'y', 'b'
 
-Vab.color, Vca.color, Vbc.color = 'c', 'y', 'm'
+Ea = Va + VRA + VXA
 
-Vab >> Vb
-Vca >> Va
-Vbc >> Vc
-# angle_V_I = -20
-# Ra = 2
-# Xa = 1
-#
-# Va = vector_r_theta(6, 0, 'r')
-# Ia = vector_r_theta(2, angle_V_I, 'c')
-# VRA = Ia * Ra
-# VXA = Ia * Xa * 1j
-# VRA.color, VXA.color = 'y', 'b'
-#
-# Ea = Va + VRA + VXA
-#
-# VRA >> Va  # Se une la cola de VRA con la cabeza de VA
-# VXA >> VRA  # Se une la cola de VXA con la cabeza de VRA
+VRA >> Va
+VXA >> VRA
 
-for vector_name in ['Va', 'Vb', 'Vc', 'Vab', 'Vca', 'Vbc']:
+for vector_name in ['Va', 'VRA', 'VXA', 'Ea', 'Ia']:
     vector = eval(vector_name)
-    plt.text(vector.xorigen + vector.x * 0.5 + 0.1 * vector.y, vector.yorigen + vector.y * 0.5 - 0.1 * vector.x, vector_name)
+    plt.text(vector.xorigen + vector.x * 0.5 + 0.1, vector.yorigen + vector.y * 0.5 + 0.1, vector_name)
     vector.plot()
 
-plt.xlim(-8, 8)
-plt.ylim(-8, 8)
+plt.xlim(-1, 15)
+plt.ylim(-1, 10)
 
 # (Ia * 4j).plot()
 
