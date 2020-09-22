4e8e4bd8bb60a39efd9f79dbbaa8846c505f9c51
DavidUrbaez <durbaezl@unal.edu.co>
2020-09-22 15:40:28 -0500

    Cambio a manejo full imaginario

--- Fasor_Graph/Fasor_Graph.py
+++ Fasor_Graph/Fasor_Graph.py
@@ -9,7 +9,7 @@ cos = lambda x: np.cos(x * np.pi / 180)
 
 # Se define la clase de Vector
 class Vector:
-    def __init__(self, xorigen=0, yorigen=0, x=None, y=None, color='b'):
+    def __init__(self, z=0, color='b'):
         '''
         Se agregan los parametros del vector
         :param xorigen: coordenada x de la cola del vector
@@ -18,9 +18,8 @@ class Vector:
         :param y: coordenada y de la cabeza del vector
         :param color: color del vector
         '''
-        self.xorigen = xorigen
-        self.yorigen = yorigen
-        self.x, self.y = x, y
+        self.zorigen=0
+        self.z=z
         self.color = color
 
     def __rshift__(self, other):
@@ -29,33 +28,35 @@ class Vector:
         :param other: Vector nuevo de ubicación origen
         :return: Vector con origen desfasado
         '''
-        self.xorigen = other.xorigen + other.x
-        self.yorigen = other.yorigen + other.y
+        self.zorigen=other.zorigen+other.z
+
         return self
 
-    def __add__(self, other, color='g'):
-        suma = Vector(self.xorigen, self.yorigen, self.x, self.y, color)
-        suma.x = self.x + other.x
-        suma.y = self.y + other.y
-        return suma
-
-    def __sub__(self, other, color='c'):
-        resta = Vector(self.xorigen, self.yorigen, self.x, self.y, color)
-        resta.x = self.x - other.x
-        resta.y = self.y - other.y
-        return resta
-
-    def __mul__(self, k):
-        mul = Vector(self.xorigen, self.yorigen, self.x, self.y, self.color)
-        mul.x = ((self.x + self.y * 1j) * k).real
-        mul.y = ((self.x + self.y * 1j) * k).imag
-        return mul
+    # def __add__(self, other, color='g'):
+    #     suma = Vector(self.xorigen, self.yorigen, self.x, self.y, color)
+    #     suma.x = self.x + other.x
+    #     suma.y = self.y + other.y
+    #     return suma
+    #
+    # def __sub__(self, other, color='c'):
+    #     resta = Vector(self.xorigen, self.yorigen, self.x, self.y, color)
+    #     resta.x = self.x - other.x
+    #     resta.y = self.y - other.y
+    #     return resta
+    #
+    # def __mul__(self, k):
+    #     mul = Vector(self.xorigen, self.yorigen, self.x, self.y, self.color)
+    #     mul.x = ((self.x + self.y * 1j) * k).real
+    #     mul.y = ((self.x + self.y * 1j) * k).imag
+    #     return mul
 
     def plot(self):
         '''
         Se grafica el vector en cuestion
         '''
-        plt.quiver(self.xorigen, self.yorigen, self.x, self.y, angles='xy', scale_units='xy', scale=1, color=self.color)
+        plt.quiver(self.zorigen.real, self.zorigen.imag,
+                   self.z.real, self.z.imag,
+                   angles='xy', scale_units='xy', scale=1, color=self.color)
 
 
 def vector_r_theta(r, theta, color='k'):
